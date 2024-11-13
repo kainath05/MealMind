2ae296891d4f8aff0dd9618b41337e6a64658661
kainath05 <kainath_ahmed@hotmail.com>
2024-11-13 10:55:53 -0500

    Revert "Merge branch 'Milestone2(File-structure-commit)' of https://github.com/kainath05/MealMind into Milestone2(File-structure-commit)"
    This reverts commit d949cb20a3c9d6b8b4db8d0824d13d71f16f9eb9, reversing
    changes made to 8672a138ad82744cafd101e1617262128d6cc500.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.devtools.ksp")
-    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.example.mealmind"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mealmind"
@@ -61,8 +59,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.room.common)
-    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -70,30 +66,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-
-    implementation(libs.androidx.room.runtime)
-    annotationProcessor(libs.androidx.room.compiler)
-    ksp(libs.androidx.room.compiler)
-
-
-
-    // optional - Kotlin Extensions and Coroutines support for Room
-    implementation(libs.androidx.room.ktx)
-
-    // optional - RxJava2 support for Room
-    implementation(libs.androidx.room.rxjava2)
-
-    // optional - RxJava3 support for Room
-    implementation(libs.androidx.room.rxjava3)
-
-    // optional - Guava support for Room, including Optional and ListenableFuture
-    implementation(libs.androidx.room.guava)
-
-    // optional - Test helpers
-    testImplementation(libs.androidx.room.testing)
-
-    // optional - Paging 3 Integration
-    implementation(libs.androidx.room.paging)
 }
-
--- app/src/main/java/com/example/mealmind/MainActivity.kt
+++ app/src/main/java/com/example/mealmind/MainActivity.kt
@@ -11,27 +11,37 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.room.Room
-import com.example.mealmind.components.ScaffoldTopBar
-import com.example.mealmind.data.database.AppDatabase
-import com.example.mealmind.screens.RegisterScreenStateful
 import com.example.mealmind.ui.theme.MealMindTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContent {
             MealMindTheme {
-                Scaffold(
-                    topBar = { ScaffoldTopBar() },
-                    content = { paddingValues ->
-                        RegisterScreenStateful(Modifier.padding(paddingValues))
-                    }
-                )
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MealMindTheme {
+        Greeting("Android")
+    }
+}
