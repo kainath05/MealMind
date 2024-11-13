26fdfc3380537d2ef05fe08492b75154e63648f5
kainath05 <kainath_ahmed@hotmail.com>
2024-11-13 10:55:50 -0500

    Reapply "Merge branch 'Milestone2(File-structure-commit)' of https://github.com/kainath05/MealMind into Milestone2(File-structure-commit)"
    This reverts commit 2bad0a280d185a022f178e7c671455749a80fca6.

--- app/src/main/java/com/example/mealmind/ui/theme/Type.kt
+++ app/src/main/java/com/example/mealmind/ui/theme/Type.kt
@@ -2,33 +2,41 @@ package com.example.mealmind.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.mealmind.R
+
+val tilt_neon = FontFamily(
+    Font(R.font.tiltneon_regular)
+)
+
+val cabin = FontFamily(
+    Font(R.font.cabin_regular),
+    Font(R.font.cabin_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = tilt_neon,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 40.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = tilt_neon,
+        fontWeight = FontWeight.Bold,
+        fontSize = 34.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
     )
-    */
 )
