2bad0a280d185a022f178e7c671455749a80fca6
kainath05 <kainath_ahmed@hotmail.com>
2024-11-13 10:55:48 -0500

    Revert "Merge branch 'Milestone2(File-structure-commit)' of https://github.com/kainath05/MealMind into Milestone2(File-structure-commit)"
    This reverts commit d949cb20a3c9d6b8b4db8d0824d13d71f16f9eb9, reversing
    changes made to 8672a138ad82744cafd101e1617262128d6cc500.

--- app/src/main/java/com/example/mealmind/components/ScaffoldTopBar.kt
+++ app/src/main/java/com/example/mealmind/components/ScaffoldTopBar.kt
@@ -1,33 +1,4 @@
 package com.example.mealmind.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ScaffoldTopBar() {
-    TopAppBar(
-        title = {
-            Box(
-                modifier = Modifier.fillMaxWidth(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "MealMind",
-                    style = MaterialTheme.typography.displayLarge
-                )
-            }
-        },
-        colors = TopAppBarDefaults.smallTopAppBarColors(
-            titleContentColor = MaterialTheme.colorScheme.primary
-        )
-    )
+class ScaffoldTopBar {
 }
